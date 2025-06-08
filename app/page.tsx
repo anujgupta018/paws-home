0d08ecf699e74eaad8bc2f827ac6ee47febb74ee
Anuj <invincibleanuj1718@gmail.com>
2025-06-09 00:17:31 +0530

    Updated adoption section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { FeaturedPets } from "@/components/featured-pets";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Select,
   SelectContent,
@@ -7,7 +9,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Heart, Search, Users } from "lucide-react";
+import { Award, Heart, PawPrint, Search, Users } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,12 +21,16 @@ export default function Home() {
           <div className="grid lg:grid-cols-2 gap-6 lg:gap-12 items-center">
             <div className="space-y-4">
               <div className="inline-block">
-                <Badge variant="secondary" className="mb-2 ">
+                <Badge
+                  variant="secondary"
+                  className="mb-2 text-lg bg-gradient-to-l from-blue-300 to-orange-300
+                text-transparent bg-clip-text"
+                >
                   Find Your perfect Companion
                 </Badge>
               </div>
               <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter">
-                Give a Pet a <span className="text-primary">Forever Home</span>
+                Give a Pet a <span className="text-red-300">Forever Home</span>
               </h1>
               <p className="text-xl text-muted-foreground md:w-[85%]">
                 Connect with loving pets in need of homes. Our mission is to
@@ -71,7 +77,7 @@ export default function Home() {
                 <Button size="lg" asChild>
                   <Link href="/pets">Browse All Pets</Link>
                 </Button>
-                <Button>
+                <Button asChild size="lg" variant="outline">
                   <Link href="/adoption">Learn About Adoption</Link>
                 </Button>
               </div>
@@ -85,23 +91,130 @@ export default function Home() {
                 className="rounded-lg shadow-xl mx-auto"
                 priority
               />
-              <div className="absolute -top-4 -left-4 bg-background p-4 rounded-lg shadow-lg
-              hidden md:flex items-center space-x-2">
-                <Heart className="h-5 w-5 text-red-500"/>
+              <div
+                className="absolute -top-4 -left-4 bg-background p-4 rounded-lg shadow-lg
+              hidden md:flex items-center space-x-2"
+              >
+                <Heart className="h-5 w-5 text-red-500" />
                 <div>
                   <div className="font-bold">2845</div>
-                  <div className="text-xs text-muted-foreground">Pets Adopted</div>
+                  <div className="text-xs text-muted-foreground">
+                    Pets Adopted
+                  </div>
                 </div>
               </div>
-              <div className="absolute -bottom-4 -right-4 bg-background p-4 rounded-lg shadow-lg hidden md:flex
-              items-center space-x-2">
-                  <Users className="h-4 w-4 text-blue-500"/>
-                  <div>
-                    <div className="font-bold">1234</div>
-                    <div className="text-xs text-muted-foreground">Happy Families</div>
+              <div
+                className="absolute -bottom-4 -right-4 bg-background p-4 rounded-lg shadow-lg hidden md:flex
+              items-center space-x-2"
+              >
+                <Users className="h-4 w-4 text-blue-500" />
+                <div>
+                  <div className="font-bold">1234</div>
+                  <div className="text-xs text-muted-foreground">
+                    Happy Families
                   </div>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      {/* Featured Pets section now */}
+      <section className="py-16 md:py-24">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center mb-12 text-center">
+            <Badge
+              variant="outline"
+              className="mb-4 bg-gradient-to-r from-red-300 to-yellow-500 text-transparent bg-clip-text"
+            >
+              Available Now
+            </Badge>
+            <h2 className="text-3xl md:text-4xl font-bold tracking-tighter mb-4">
+              Meet Your New Bestfriend
+            </h2>
+            <p className="text-xl text-muted-foreground max-w-[85%] md:max-w-[65%]">
+              These adorable pets are ready to find their forever homes. Each
+              one has been health checked and is waiting for a loving family.
+            </p>
+          </div>
+
+          <FeaturedPets />
+
+          <div className="text-center mt-12">
+            <Button size="lg" variant="outline" asChild>
+              <Link href="/pets">
+                View All Available Pets
+                <PawPrint className="h-2 w-2 text-yellow-700" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      {/* {Adoption Procees Section} */}
+      <section className="py-16 md:py-24 bg-muted/50">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center text-center mb-12">
+            <h2 className="text-3xl md:text-4xl font-bold tracking-tighter mb-4">
+              Simple Adoption Process
+            </h2>
+            <p className="text-xl text-muted-foreground max-w-[85%] md:max-w-[65%]">
+              We've made adoption a pet as easy as possible while ensuring the
+              best match for you and your companion.
+            </p>
+          </div>
+
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+            {[
+              {
+                step: "1",
+                title: "Browse and Search",
+                description:
+                  "Find pets that match your lifestyle and preferences using our advanced search filters.",
+                icon: Search,
+              },
+              {
+                step: "2",
+                title: "Meet and Greet",
+                description:
+                  "Schedule a visit to meet your potential pet and see if its a perfect match.",
+                icon: Heart,
+              },
+              {
+                step: "3",
+                title: "Application",
+                description:
+                  "Complete our simple adoption application and provide references for a quick review.",
+                icon: Users,
+              },
+              {
+                step: "4",
+                title: "Welcome Home",
+                description:
+                  "Take your new best friend home and start your beautiful journey together.",
+                icon: Award,
+              },
+            ].map((step, index) => (
+              <Card key={index} className="text-center">
+                <CardHeader>
+                  <div className="relative mx-auto mb-2">
+                    <div className="w-16 h-16 bg-primary/10 rounded-full flex items-center justify-center text-primary font-bold text-2xl">
+                      <step.icon className="h-8 w-8 " />
+                    </div>
+                    <div
+                      className="absolute -top-2 -right-2 w-8 h-8 rounded-full text-muted-foreground
+                     flex items-center justify-center font-bold text-sm"
+                    >
+                      {step.step}
+                    </div>
+                  </div>
+                  <CardTitle>{step.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-muted-foreground">{step.description}</p>
+                </CardContent>
+              </Card>
+            ))}
           </div>
         </div>
       </section>
